bb0fa243b8061330f34653601ddf44663705445f
salehmorovat <126867033+salehmorovat@users.noreply.github.com>
2023-11-06 00:19:16 +0330

    my list project
    projeckt my list

---
+++ my_list.py
@@ -0,0 +1,64 @@
+# Welcome to my list project
+ # لیست اول: نام‌های پسر
+boys_name_list = ["Mani", "Mohammad", "Ali", "Amir Hossein", "Hossein", "Abolfazl", "Amir Abbas", "Samiyar", "Mohammad Taha", "Mohammad Reza"]
+for boys_name in boys_name_list :
+    print ( boys_name )
+
+
+# لیست دوم: نام‌های حیوانات
+animals_list = ["Lion", "Tiger", "Elephant", "Giraffe", "Dolphin", "Panda", "Kangaroo", "Zebra", "Cheetah", "Penguin"]
+for animals in animals_list :
+    print ( animals )
+
+
+# لیست سوم: نام‌های شهرها
+cities_list = ["New York", "Los Angeles", "Paris", "Tokyo", "London", "Sydney", "Dubai", "Rome", "Berlin", "Moscow"]
+for cities in cities_list :
+    print ( cities )
+
+
+# لیست چهارم: نام‌های میوه‌ها
+fruits_list = ["Apple", "Banana", "Orange", "Grapes", "Strawberry", "Watermelon", "Mango", "Pineapple", "Cherry", "Kiwi"]
+for fruits in fruits_list :
+    print ( fruits )
+
+
+# لیست پنجم: نام‌های رنگ‌ها
+colors_list = ["Red", "Blue", "Green", "Yellow", "Purple", "Orange", "Pink", "Brown", "Black", "White"]
+for colors in colors_list :
+    print ( colors )
+
+
+# لیست ششم: نام‌های ورزش‌ها
+sports_list = ["Football", "Basketball", "Tennis", "Soccer", "Golf", "Swimming", "Cricket", "Baseball", "Volleyball", "Hockey"]
+for sports in sports_list :
+    print ( sports )
+
+
+# لیست هفتم: نام‌های میهن‌ها
+countries_list = ["Iran", "USA", "France", "Japan", "UK", "Australia", "UAE", "Italy", "Germany", "Russia"]
+for countries in countries_list :
+    print ( countries )
+
+
+# لیست هشتم: نام‌های اساتید
+teachers_list = ["Professor Smith", "Dr. Johnson", "Professor Lee", "Dr. Clark", "Professor Brown", "Dr. Wilson", "Professor Taylor", "Dr. Anderson", "Professor Harris", "Dr. Martin"]
+for teachers in teachers_list :
+    print ( teachers )
+
+
+
+
+# لیست نهم: نام‌های ماه‌های سال
+months_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October"]
+for months in months_list :
+    print ( months )
+
+
+
+# لیست دهم: نام‌های ماشین‌ها
+cars_list = ["Toyota", "Honda", "Ford", "Chevrolet", "Nissan", "BMW", "Mercedes-Benz", "Audi", "Lexus", "Tesla"]
+for cars in cars_list:
+    print ( cars ) 
+
+
